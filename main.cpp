34c72984752445e97aee100c814da733d2cbda54
futurepr0n <mark@futurepr0n.com>
2020-05-29 13:26:05 -0400

    first lesson

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    cout << "Hello and Welcome!\n";
+    string name;
+    string age;
+    cout << "What is your name? ";
+    cin >> name;
+    cout << "And what is your age? ";
+    cin >> age;
+    cout << "Ah nice to meet you "<< name << "! Wow, " << age << " is quite a nice age to be!\n";
+    
+}
