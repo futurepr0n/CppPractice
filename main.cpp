8357fa6fee42ee8b51f5b971c79ad7da8624116a
futurepr0n <mark@futurepr0n.com>
2020-05-30 16:30:12 -0400

    vector end

--- main.cpp
+++ main.cpp
@@ -7,7 +7,16 @@ int main(){
     
     vector<string> inventory;
 
-    
+    inventory.push_back("Sword");
+    inventory.push_back("Sheild");
+
+    inventory.pop_back();
+    int size = inventory.size();
+    inventory.front();
+    inventory.back();
+    inventory.clear();
+
+    cout << "What is the size " << size << "\n"; 
 
 
 
