e3e45cdf0b2297c6a03385cf6cbd9215ffce417b
futurepr0n <mark@futurepr0n.com>
2020-05-30 14:19:50 -0400

    nice pointer example with more context

--- main.cpp
+++ main.cpp
@@ -7,7 +7,23 @@ int main(){
     int someInt = 1;
     int *ptr = &someInt;
     
-    cout << "This is the memory Address of " << ptr << "\n";
-    cout << "And this is the value "<< *ptr << "\n";
- 
+    cout << "This is the memory Address of ptr " << ptr << "\n";
+    cout << "And this is the value *ptr "<< *ptr << "\n";
+
+    int someOtherInt = someInt;
+
+    cout << "This is the value of someOtherInt " << someOtherInt << " before we changed someInt's value \n";
+    someInt = 5;
+
+    cout << "This is the value of someOtherInt " << someOtherInt << " after we changed someInt's value to 5 \n";
+
+    cout << "Now with pointers \n";
+    cout <<"You can see that the reference to *ptr is changing with the address " << *ptr <<"\n";
+
+
+
+
+
+
+
  }
