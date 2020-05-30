d13704a1d72722c194e60b4ca4ea062beb8267fa
futurepr0n <mark@futurepr0n.com>
2020-05-30 16:26:04 -0400

    vectors start

--- main.cpp
+++ main.cpp
@@ -1,24 +1,13 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 int main(){
-    string inventory[3];
-    inventory[0] = "Sword";
-    inventory[1] = "Shields";
-    inventory[2] = "Boots";
+    
+    vector<string> inventory;
 
-    string first = inventory[0];
-
-    cout << "The first Item is: "<< first << "\n";
-
-    string inventory2[]={"item 1", "item 2", "item 3"};
-
-    cout << "Inventory address is " << inventory2 << "\n";
-
-    cout << "Inventory items: #1 "<< inventory2[0] << "\n";
-    cout << "Inventory items: #2 "<< inventory2[1] << "\n";
-    cout << "Inventory items: #3 "<< inventory2[2] << "\n";
+    
 
 
 
