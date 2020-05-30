39e32803678f1f637c0e12646b9b8aaacf40d7f0
futurepr0n <mark@futurepr0n.com>
2020-05-30 15:01:19 -0400

    operators part 2

--- main.cpp
+++ main.cpp
@@ -3,10 +3,11 @@
 using namespace std;
 
 int main(){
+
+    int currentHealth;
  
     int maxHealth = 100;
-    int currentHealth = maxHealth;
-
+    currentHealth = maxHealth;
     bool isGameOver = false;
 
     isGameOver = !isGameOver;
@@ -31,7 +32,56 @@ int main(){
     cout << welcome + playerName;
 
 
+    currentHealth = 50;
 
+    bool isTrue = currentHealth <= maxHealth;
+
+    cout << "The max Health is: " << maxHealth << "\n";
+    cout << "The current Health is: " << currentHealth << "\n";
+    cout << "Now with isTrue = currentHealth <= maxHealth: " << isTrue << "\n";
+
+    isTrue = currentHealth == maxHealth;
+    cout << "Now with isTrue = currentHealth == maxHealth: " << isTrue << "\n";
+
+    isTrue = currentHealth >= maxHealth;
+    cout << "Now with isTrue = currentHealth >= maxHealth: " << isTrue << "\n";
+
+    isTrue = currentHealth != maxHealth;
+    cout << "Now with isTrue = currentHealth != maxHealth: " << isTrue << "\n";
+
+    bool otherIsTrue = isTrue == true;
+    cout << "otherIsTrue = isTrue == true: " << otherIsTrue << "\n";
+
+    otherIsTrue = isTrue == false;
+    cout << "otherIsTrue = isTrue == false: " << otherIsTrue << "\n";
+
+    otherIsTrue = isTrue;
+    cout << "otherIsTrue = isTrue: " << otherIsTrue << "\n";
+
+    otherIsTrue = !isTrue;
+    cout << "otherIsTrue = !isTrue " << otherIsTrue << "\n";
+
+    string firstName = "Mark";
+    string lastName = "Pereira";
+
+    isTrue = firstName == lastName;
+
+    cout << "Now with isTrue = firstName == lastName: " << isTrue << "\n";
+
+    isTrue = firstName != lastName;
+    cout << "Now with isTrue = firstName != lastName: " << isTrue << "\n";
     
+
+    isTrue = currentHealth != maxHealth && firstName == lastName;
+    cout << "isTrue = currentHealth != maxHealth && firstName == lastName: " << isTrue << "\n";
+
+    isTrue = currentHealth != maxHealth && firstName != lastName;
+    cout << "isTrue = currentHealth != maxHealth && firstName != lastName: " << isTrue << "\n";
+
+    isTrue = currentHealth == maxHealth && firstName == lastName;
+    cout << "isTrue = currentHealth == maxHealth && firstName == lastName: " << isTrue << "\n";
+
+    isTrue = currentHealth == maxHealth && firstName != lastName;
+    cout << "isTrue = currentHealth == maxHealth && firstName != lastName: " << isTrue << "\n";
 
  }
