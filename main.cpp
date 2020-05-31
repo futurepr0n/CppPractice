b46f450abc6bc1264587e8761953788123a79625
futurepr0n <mark@futurepr0n.com>
2020-05-31 17:46:02 -0400

    new class

--- main.cpp
+++ main.cpp
@@ -4,17 +4,39 @@
 using namespace std;
 
 class GameCharacter {
-    public:
-        int maxHealth, currentHealth, attack, defence;
-        GameCharacter(int, int, int);
-        void takeDamage(int);
+public:
+    int maxHealth, currentHealth, attack, defence;
+    GameCharacter(int, int, int);
+    void takeDamage(int);
 
+};
+
+GameCharacter::GameCharacter(int h, int a, int d) {
+
+    maxHealth = h;
+    currentHealth = h;
+    attack = a;
+    defence = d;
+
+}
+
+void GameCharacter::takeDamage(int a){
+    int damage = a - defence;
+
+    if(damage < 0){
+        damage = 0;
+    }
+    currentHealth -= damage;
 }
 
 int main(){
     
+    GameCharacter gc = GameCharacter(100,20,10);
+    cout << gc.currentHealth << "\n";
+    gc.takeDamage(50);
+    cout << gc.currentHealth << "\n";
 
-    
+
  
  }
 
