424cf3cb89e2d1c17637fb48e427f9a44831af7b
futurepr0n <mark@futurepr0n.com>
2020-05-31 18:00:18 -0400

    finished course

--- main.cpp
+++ main.cpp
@@ -29,7 +29,29 @@ void GameCharacter::takeDamage(int a){
     currentHealth -= damage;
 }
 
+class Player: public GameCharacter {
+public:
+    string name;
+    vector<string> inventory;
+    Player(string, int, int, int);
+    void addItem(string);
+
+
+}
+
+Player::Player(string n, int h, int a, int d):GameCharacter(h,a,d){
+    name = n;
+}
+
+void Player::addItem(string i){
+    inventory.push_back(i);
+}
+
 int main(){
+    
+
+    Player p = Player("Mark", 120, 30, 20);
+    p.addItem("Boots");
     
     GameCharacter gc = GameCharacter(100,20,10);
     cout << gc.currentHealth << "\n";
